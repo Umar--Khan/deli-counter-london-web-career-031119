3e1f71bdbd323724ade9da7a3302bf373cec018d
Umar Khan <umarkhandev@gmail.com>
2019-02-28 15:16:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,6 +26,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   elsif katz_deli.size >= 1
       puts "Currently serving #{katz_deli[0]}."
-      katz_deli.shift()
+      katz_deli.shift
   end
 end
